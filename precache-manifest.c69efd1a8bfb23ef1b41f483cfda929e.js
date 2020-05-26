d6682f1d1ca0083f66a94167255920485b58b36f
Serg Bara <baraserg@gmail.com>
2020-05-26 22:41:48 +0300

    Updates

---
+++ precache-manifest.c69efd1a8bfb23ef1b41f483cfda929e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2b69f31c0fda6229d65ce9311a4c14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccdaaeab6583788055f4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "66a02b6ce170555f6f2e",
+    "url": "/static/js/2.d978f30c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d978f30c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccdaaeab6583788055f4",
+    "url": "/static/js/main.a8df9daa.chunk.js"
+  },
+  {
+    "revision": "87e9909480aa5b80e6e7",
+    "url": "/static/js/runtime-main.dbab80a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
