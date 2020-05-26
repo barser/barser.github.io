298c13062fe190e921a6423c3bf9501a3b5b999d
Serg Bara <baraserg@gmail.com>
2020-05-26 23:02:59 +0300

    Изменен текст на главной странице.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Домашняя страничка на реконструкции.
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
